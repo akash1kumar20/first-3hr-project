267f111f6404ed6ab89a2f117f3564cbaf464d09
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-06-01 14:18:31 +0530

    Seller Admin Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AdminPage from "./Components/AdminPage";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AdminPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
